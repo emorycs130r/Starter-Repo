0d8c838d68f57f187b616235f107ff297c855124
Vishwanath Seshagiri <vishwa.nath@outlook.com>
2021-01-13 23:17:51 +0000

    Added more tests

--- test_hello_world.py
+++ test_hello_world.py
@@ -3,6 +3,6 @@ from hello_world import hello_world
 def test_hw():
     assert hello_world() == "Hello World!"
 
-def test_hw():
+def test_hw_1():
     value = "Hello World!"
     assert len(hello_world()) == len(value)
